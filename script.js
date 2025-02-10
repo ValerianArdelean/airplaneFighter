d86883d192edc7c77c466aece418ddb5bb4a93ec
ValerianArdelean <valerian_ardelean@yahoo.com>
2025-02-10 23:33:35 +0200

    Creating the app

---
+++ script.js
@@ -0,0 +1,113 @@
+function createCells() {
+	let table = document.getElementById("table");
+	for (let i = 10; i < 24; ++i) {
+		let line = document.createElement("div");
+		line.style.display = "flex";
+		line.id = `${i}`;
+		for (let j = 10; j < 30; ++j) {
+			let cell = document.createElement("div");
+			cell.style.width = "50px";
+			cell.style.height = "50px";
+			//cell.style.border = "1px solid #87CEEB";
+			cell.id = `${i}${j}`;
+			line.appendChild(cell);
+		}
+		table.appendChild(line);
+	}
+}
+
+createCells();
+let lines = 17;
+let columns = 21;
+let lives = 10;
+
+function processLines(n) {
+	if (lives <= 0) {
+		return;
+	}
+	n = parseInt(n);
+	if (lines + n < 21 && lines + n >= 10) {
+		erasePlane();
+		lines += n;
+		drawPlane();
+	}
+}
+
+function processColumns(n) {
+	if (lives <= 0) {
+		return;
+	}
+	n = parseInt(n);
+	if (columns + n < 28 && columns + n >= 12) {
+		erasePlane();
+		columns += n;
+		drawPlane();
+	}
+}
+
+function selectCell(l, c) {
+	return document.getElementById(`${l}` + `${c}`);
+}
+
+function erasePlane() {
+	for (let i = 0; i <= 3; ++i) {
+		selectCell(lines + i, columns).removeAttribute("class");
+	}
+	for (let i = columns - 2; i <= columns + 2; ++i) {
+		selectCell(lines + 1, i).removeAttribute("class");
+		if (i > columns - 2 && i < columns + 2) {
+			selectCell(lines + 3, i).removeAttribute("class");
+		}
+	}
+}
+
+function drawPlane() {
+	for (let i = 0; i <= 3; ++i) {
+		selectCell(lines + i, columns).classList.add("color");
+	}
+	for (let i = columns - 2; i <= columns + 2; ++i) {
+		selectCell(lines + 1, i).classList.add("color");
+		if (i > columns - 2 && i < columns + 2) {
+			selectCell(lines + 3, i).classList.add("color");
+		}
+	}
+}
+
+let l = document.getElementById("lives");
+l.innerHTML = `lives: ${lives}`;
+
+drawPlane();
+
+function bullets() {
+	for (let j = 0; j < 100000; ++j) {
+		setTimeout(() => {
+			if (lives <= 0) {
+				return;
+			}
+			let boolet = Math.floor(Math.random() * (29 - 10 + 1)) + 10;
+			selectCell(10, boolet).classList.add("color");
+			for (let i = 10; i <= 23; ++i) {
+				setTimeout(() => {
+					selectCell(i, boolet).removeAttribute("class");
+					if (selectCell(i + 1, boolet).classList.contains("color")) {
+						--lives;
+						l.innerHTML = `lives: ${lives}`;
+						if (lives <= 0) {
+							gameOver = true; // ✅ Prevent further bullets
+							l.innerHTML = "GAME OVER!";
+							return;
+						}
+					}
+					selectCell(i + 1, boolet).classList.add("color");
+				}, (i - 10) * 150);
+			}
+		}, j * 600);
+	}
+}
+bullets();
+
+
+			   
+
+
+
